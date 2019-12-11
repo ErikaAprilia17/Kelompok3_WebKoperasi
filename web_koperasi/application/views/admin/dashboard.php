73d086766efb3275bf21bbdf412e9dfde917fb1a
reginanin <reginanindyahidayat14@gmail.com>
2019-12-11 14:28:25 +0700

    controllers,view dashboard commit

---
+++ web_koperasi/application/views/admin/dashboard.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css" >
+<script src="<?php echo base_url()?>assets/js/bootstrap.min.js" ></script>
+
+	<title>Koperasi Simpan Pinjam MU | www.koperasimpanpinjam.com</title>
+
+</head>
+
+<body style="background-image: url('<?php echo base_url()?>file/a.jpg');" class="pt-5">
+
+	<center><h1><font color="black" > :: Selamat Datang !! :: </h1></font></center>
+	<center><h2><font color="black" class="mt-4">Koperasi Simpan Pinjam Mitra Usaha </font></h2></center>
+	<center><img src="<?php echo base_url().'file/vector.png'?>" class="mt-4" width="300px"/></center>
+
+    <center><a href="<?php echo base_url()?>login/login_admin" class="btn btn-primary mt-5">Login</a> 
+
+  	<a href="<?php echo base_url()?>sign_up" class="btn btn-secondary mt-5 ">Sign Up</a></center>
+
+</body>
+</html>
---
+++ web_koperasi/application/views/admin/dashboard_v.php
@@ -0,0 +1,239 @@
+<div class="content-wrapper">
+          
+          <div class="page-header">
+            <h3 class="page-title">
+              <span class="page-title-icon bg-gradient-primary text-white mr-2">
+                <i class="mdi mdi-home"></i>                 
+              </span>
+              Selamat Datang Admin !!!!
+            </h3>
+            
+          </div>
+          <div class="row">
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card bg-gradient-danger card-img-holder text-white">
+                <div class="card-body">
+                  <img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>
+                  <div class="small-box bg-yellow">
+										<div class="inner">
+											<h2 class="kotak_judul"> Pemasukan Transaksi Kas</h2>
+											<table>
+												<tbody><tr>
+													<td class="h_kanan"><strong style="font-size: 20px;">10,000,000	</strong></td>
+													<td> &nbsp; Transaksi Bulan Ini </td>
+												</tr>
+												<tr>
+													<td class="h_kanan">
+														<strong style="font-size: 20px;">
+															110,000,000							</strong>
+													</td>
+													<td> <b>&nbsp;Pemasukan Tahun Ini</b></td>
+												</tr>
+												
+											</tbody></table>
+										</div>
+										<div class="icon">
+											<i class="fa fa-money"></i>
+										</div>
+										<a class="small-box-footer" href="<?php echo base_url()?>admin/laporan/kas_pinjaman">
+											More info
+											<i class="fa fa-arrow-circle-right"></i>
+										</a>
+									</div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card bg-gradient-info card-img-holder text-white">
+                <div class="card-body">
+                  <img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>                  
+                  <div class="small-box bg-green">
+											<div class="inner">
+												<h2 class="kotak_judul"> Simpanan Tahun 2019 </h2>
+												<table>
+													<tbody><tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">1,100,000</strong></td>
+														<td> &nbsp; Simpanan Anggota</td>
+													</tr>
+													<tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">10,000,000,030,000</strong></td>
+														<td> &nbsp; Penarikan Tunai</td>
+													</tr>
+													<tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">-9,999,998,930,000</strong></td>
+														<td> &nbsp; Jumlah Simpanan </td>
+													</tr>
+												</tbody></table>
+											</div>
+											<div class="icon">
+												<i class="ion ion-ios7-briefcase-outline"></i>
+											</div>
+											<a class="small-box-footer" href="<?php echo base_url()?>admin/laporan/kas_simpanan">
+												More info
+												<i class="fa fa-arrow-circle-right"></i>
+											</a>
+										
+								  </div>
+								 </div>
+								</div>
+							</div>
+            <div class="col-md-4 stretch-card grid-margin">
+              <div class="card bg-gradient-success card-img-holder text-white">
+                <div class="card-body">
+                  <img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>                                    
+                 <div class="small-box bg-purple">
+										<div class="inner">
+											<h2 class="kotak_judul"> Pengeluaran Transaksi Kas
+											 </h2>
+											<table>
+												<tbody><tr>
+													<td class="h_kanan"><strong style="font-size: 20px;">
+													7,000,000 
+											 	</strong></td>
+													<td> &nbsp; Pengeluaran Bulan Ini</td>
+												</tr>
+												<tr>
+													<td class="h_kanan"><strong style="font-size: 20px;">
+													80,000,000 
+													</strong></td>
+													<td> &nbsp; Pengeluaran Tahun Ini</td>
+												</tr>
+											</tbody></table>
+										</div>
+										<div class="icon">
+											<i class="fa fa-book"></i>
+										</div>
+										<a class="small-box-footer" href="<?php echo base_url()?>admin/laporan/saldo_kas">
+											More info
+											<i class="fa fa-arrow-circle-right"></i>
+										</a>
+									</div>       
+								 </div>
+							  </div>
+							 </div>
+						
+           <div class="col-md-4 stretch-card grid-margin">
+              <div class="card bg-gradient-success card-img-holder text-white">
+                <div class="card-body">
+                  <img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>                                    
+                 <div class="small-box bg-purple">
+										<div class="small-box bg-blue">
+											<div class="inner">
+												<h2 class="kotak_judul"> Data Anggota</h2>
+												<table>
+													<tbody><tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">5</strong></td>
+														<td> &nbsp; Anggota Aktif</td>
+													</tr>
+													<tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">0</strong></td>
+														<td> &nbsp; Anggota Tidak Aktif</td>
+													</tr>
+													<tr>
+														<td class="h_kanan"><strong style="font-size: 20px;">5</strong></td>
+														<td> &nbsp; Jumlah Anggota</td>
+													</tr>
+												</tbody></table>
+											</div>
+											<div class="icon">
+												<i class="ion ion-person-add"></i>
+											</div>
+											<a class="small-box-footer" href="https://www.koperasiegc.com/lap_anggota">
+												More info
+												<i class="fa fa-arrow-circle-right"></i>
+											</a>
+										</div>
+									</div>
+								 </div>
+								</div>
+							</div>
+							 <div class="col-md-4 stretch-card grid-margin">
+              <div class="card bg-gradient-success card-img-holder text-white">
+                <div class="card-body">
+                  <img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>                                    
+                 <div class="small-box bg-purple">
+										<div class="small-box bg-blue">
+											<div class="small-box bg-aqua">
+												<div class="inner">
+													<h2 class="kotak_judul"> Data Pengguna</h2>
+														<table>
+														<tbody><tr>
+															<td class="h_kanan"><strong style="font-size: 20px;">3</strong></td>
+																	<td> &nbsp; User Aktif</td>
+																</tr>
+																<tr>
+															<td class="h_kanan"><strong style="font-size: 20px;">0</strong></td>
+															<td> &nbsp; User Non-Aktif</td>
+																</tr>
+															<tr>
+															<td class="h_kanan"><strong style="font-size: 20px;">3</strong></td>
+															<td> &nbsp; Jumlah User </td>
+														</tr>
+												</tbody>
+											</table>
+											</div>
+											<div class="icon">
+												<i class="fa  fa-users"></i>
+											</div>
+											<a class="small-box-footer" href="https://www.koperasiegc.com/user">
+												More info
+												<i class="fa fa-arrow-circle-right"></i>
+											</a>
+										</div>
+									</div>
+								 </div>
+								</div>
+							</div>
+						</div>
+								 <div class="col-md-4 stretch-card grid-margin">
+             			 <div class="card bg-gradient-info card-img-holder text-white">
+                		<div class="card-body">
+                  		<img src="<?= base_url('assets') ?>/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>                  
+                  			<div class="small-box bg-green">
+													<div class="small-box bg-red">
+													<div class="inner">
+													<h2 class="kotak_judul"> Data Peminjam</h2>
+													<table>
+													<tbody>
+														<tr>
+													<td class="h_kanan"><strong style="font-size: 20px;">9</strong></td>
+													<td> &nbsp; Peminjam</td>
+													</tr>
+												<tr>
+										<td class="h_kanan"><strong style="font-size: 20px;">6</strong></td>
+										<td> &nbsp; Sudah Lunas</td>
+											</tr>
+											<tr>
+										<td class="h_kanan"><strong style="font-size: 20px;">3</strong></td>
+										<td> &nbsp; Belum Lunas </td>
+										</tr>
+										</tbody>
+										</table>
+										</div>
+										<div class="icon">
+									<i class="fa fa-calendar"></i>
+										</div>
+									<a class="small-box-footer" href="https://www.koperasiegc.com/pinjaman">
+																More info
+											<i class="fa fa-arrow-circle-right"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+          
+         
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
